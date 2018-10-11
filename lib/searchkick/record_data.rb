3816f4316e278af2784ba6e50bea85b57ce6bb01
Maxime Lepage <maxime.l@kimoby.com>
2018-10-11 14:12:14 -0400

    Creating indexes for klass and user before bulk_indexing so it does not create race condition

--- lib/searchkick/record_data.rb
+++ lib/searchkick/record_data.rb
@@ -35,8 +35,6 @@ module Searchkick
     end
 
     def record_data
-      Searchkick::Index.create_index_if_needed(record.class, record.user_id)
-
       data = {
         _index: "#{record.class.to_s.downcase.pluralize}_#{Rails.env}_#{record.user_id}",
         _id: search_id,
